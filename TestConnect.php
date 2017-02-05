dd6851d4e92877b10727b99f6bfaac3badb99329
jasonckeating <jasonckeating@hotmail.com>
2017-02-05 15:36:16 -0600

    Add files via upload

---
+++ TestConnect.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="TC.css">
+	</head>
+	
+	<body>
+		<div class="login">
+			<div class="login-triangle"></div>
+
+			<h2 class="login-header">Log in</h2>
+
+			<form class="login-container" id="login" action="home.php?question=1">
+				<p><input type="email" placeholder="Email"></p>
+				<p><input type="password" placeholder="Password"></p>
+				<p><input onclick="login()" type="submit" value="Log in"></p>
+			
+				<script>
+					function login() 
+					{
+						// Save user email and password for dynamic log in
+						
+						// If administrator...
+						
+						// If instructor...
+						
+						// If student...
+					}
+				</script>
+			</form>
+		</div>
+	</body>
+</html>
